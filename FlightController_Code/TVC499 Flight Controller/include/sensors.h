3f975a6d365016410cd26ac25503026d26c1b039
grantstec <129014437+grantstec@users.noreply.github.com>
2025-04-23 21:55:12 -0600

    idea

--- FlightController_Code/TVC499 Flight Controller/include/sensors.h
+++ FlightController_Code/TVC499 Flight Controller/include/sensors.h
@@ -12,15 +12,34 @@ class SensorSystem {
 private:
     Adafruit_BNO08x bno;
     Adafruit_BMP3XX bmp;
+
+    double gyroOffsets[3] = {0.0, 0.0, 0.0}; // Gyro offsets for calibration
+    double altData[3] = {0.0, 0.0, 0.0}; // Altitude data array
+    double quaternions[4] = {1.0, 0.0, 0.0, 0.0}; // Quaternion data
+    double accelerometer[3] = {0.0, 0.0, 0.0}; // Accelerometer data array
+    double eulerAngles[3] = {0.0, 0.0, 0.0}; // Euler angles array
+    double gyroRates[3] = {0.0, 0.0, 0.0}; // Gyroscope rates array
+
     
 public:
-    
+
     bool initialize(double* quaternions, double* accelerometer);
     void updateIMU(double* gyroRates, double* quaternions, double* eulerAngles, double* accelerometer, double dt);
     bool updateAltimeter(double* altData);
     void resetSensors(double* quaternions, double* accelerometer);
     void zeroAltimeter();
     void initializeQuaternions(double* quaternions, double* accelerometer);
+
+    //getter functions
+    double* getGyroRates() { return gyroRates; }
+    double* getQuaternions() { return quaternions; }
+    double* getEulerAngles() { return eulerAngles; }
+    double* getAccelerometer() { return accelerometer; }
+    double* getAltData() { return altData; }
+    double getAltitude() { return altData[0]; }
+    double getPressure() { return altData[1]; }
+    double getTemperature() { return altData[2]; }
+
 };
 
 #endif // SENSOR_SYSTEM_H
